ef9814bf7aedbeee25f03cfd814f2ab4c05a38f3
Stefano Savanelli <stefano@savanelli.it>
2021-07-16 15:35:33 +0200

    add themes imports

--- src/App.js
+++ src/App.js
@@ -13,7 +13,40 @@ import Routes from './Routes'
 
 import PrimeReact from 'primereact/api';
 
-import 'primereact/resources/themes/saga-blue/theme.css';
+// import 'primereact/resources/themes/bootstrap4-light-blue/theme.css'
+// import 'primereact/resources/themes/bootstrap4-light-purple/theme.css'
+// import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css'
+// import 'primereact/resources/themes/bootstrap4-dark-purple/theme.css'
+// import 'primereact/resources/themes/md-light-indigo/theme.css'
+// import 'primereact/resources/themes/md-light-deeppurple/theme.css'
+// import 'primereact/resources/themes/md-dark-indigo/theme.css'
+// import 'primereact/resources/themes/md-dark-deeppurple/theme.css'
+// import 'primereact/resources/themes/mdc-light-indigo/theme.css'
+// import 'primereact/resources/themes/mdc-light-deeppurple/theme.css'
+// import 'primereact/resources/themes/mdc-dark-indigo/theme.css'
+// import 'primereact/resources/themes/mdc-dark-deeppurple/theme.css'
+// import 'primereact/resources/themes/fluent-light/theme.css'
+import 'primereact/resources/themes/saga-blue/theme.css'
+// import 'primereact/resources/themes/saga-green/theme.css'
+// import 'primereact/resources/themes/saga-orange/theme.css'
+// import 'primereact/resources/themes/saga-purple/theme.css'
+// import 'primereact/resources/themes/vela-blue/theme.css'
+// import 'primereact/resources/themes/vela-green/theme.css'
+// import 'primereact/resources/themes/vela-orange/theme.css'
+// import 'primereact/resources/themes/vela-purple/theme.css'
+// import 'primereact/resources/themes/arya-blue/theme.css'
+// import 'primereact/resources/themes/arya-green/theme.css'
+// import 'primereact/resources/themes/arya-orange/theme.css'
+// import 'primereact/resources/themes/arya-purple/theme.css'
+// import 'primereact/resources/themes/nova/theme.css'
+// import 'primereact/resources/themes/nova-alt/theme.css'
+// import 'primereact/resources/themes/nova-accent/theme.css'
+// import 'primereact/resources/themes/luna-amber/theme.css'
+// import 'primereact/resources/themes/luna-blue/theme.css'
+// import 'primereact/resources/themes/luna-green/theme.css'
+// import 'primereact/resources/themes/luna-pink/theme.css'
+// import 'primereact/resources/themes/rhea/theme.css'
+
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
